d948bed7d6a60fef40944b439f54b0177864b210
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-06-20 15:39:02 -0300

    images on image folder

--- src/lib/Cards.svelte
+++ src/lib/Cards.svelte
@@ -1,42 +1,56 @@
 <script>
-    export let cards = [];
+export let cards = [];
 </script>
 
 <style>
-    .cards {
-        height: fit-content;
-        display: flex;
-        margin-top: 100px;
-        gap: 5px;
-    }
+.cards {
+    height: fit-content;
+    display: flex;
+    margin-top: 100px;
+    gap: 5px;
+}
 
-    .cards .card {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-        text-align: center;
-        height: 150px;
-        width: 30px;
-        border-radius: 10px;
-        color: white;
-        transition: 400ms;
-    }
+.cards .card {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    height: 150px;
+    width: 30px;
+    border-radius: 10px;
+    color: white;
+    transition: 400ms;
+    background-size: cover;
+    background-position: center;
+}
 
-    .cards .card:hover {
-        width: 330px;
-        background-size: cover;
-        background-position: center;
-    }
+.cards .card:hover {
+    width: 330px;
+}
 
-    .cards:hover > .card:not(:hover) {
-        filter: blur(10px);
-        transform: scale(0.9, 0.9);
-    }
+.cards .card:hover img {
+    display: block;
+}
+
+.cards:hover > .card:not(:hover) {
+    filter: blur(10px);
+    transform: scale(0.9, 0.9);
+}
+
+.card img {
+    display: none;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+}
 </style>
 
 <div class="cards">
-    {#each cards as { color, hoverImage }}
-        <div class="card" style="background-color: {color}; background-image: url({hoverImage})"></div>
-    {/each}
+{#each cards as { color, hoverImage }}
+    <div class="card" style="background-color: {color}">
+    <img src={hoverImage} alt="hover image" />
+    </div>
+{/each}
 </div>
+  
