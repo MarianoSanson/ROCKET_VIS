8ab70a246876865fa9704c711e72086d3cf7885b
Wolo-N <nicolas.wolodarskyfranke@gmail.com>
2024-07-09 22:42:15 -0300

    emprolijando scroll

--- src/lib/SixthSection.svelte
+++ src/lib/SixthSection.svelte
@@ -42,7 +42,6 @@ onMount(() => {
 </script>
 
 <main>
-    <div class="overlay-container">
 <section class="sixth_section">
     <h1 id="section_header">FICHA TECNICA</h1>
     <div bind:this={scrollContainer} class="scroll-container">
@@ -50,12 +49,13 @@ onMount(() => {
         <h1 class="technic_head">Dirigida Por</h1>
         <div class="row">
         <h3>Stanley Kubrick</h3>
-        <img src="/images/stanley-kubrick.png" alt="Stanley Kubrick" />
+        <h3>&#8594;</h3>
         </div>
+        <img src="/images/stanley-kubrick.png" alt="Stanley Kubrick" />
     </div>
 
     <div class="section-content Genero">
-        <h1>Género</h1>
+        <h1 class="technic_head">Genero</h1>
         <h3>Ciencia Ficción</h3>
         <div class="row">
         <img src="/images/backshot-1.png" alt="backshot-1">
@@ -64,21 +64,19 @@ onMount(() => {
     </div>
 
     <div class="section-content Duracion-Año">
-        <div class="row">
         <div class="release_year">
-            <h1>Año de lanzamiento</h1>
+            <h1 class="technic_head">Año de lanzamiento</h1>
             <h3 class="counter" data-target="1968">1968</h3>
         </div>
 
         <div class="movie_length">
-            <h1>Duración</h1>
+            <h1 class="technic_head">Duración</h1>
             <h3 class="counter" data-target="142" data-suffix=" minutos">142 minutos</h3>
-        </div>
         </div>
     </div>
 
     <div class="section-content Presupuesto">
-        <h1>Presupuesto</h1>
+        <h1 class="technic_head">Presupuesto</h1>
         <div class="row">
         <button class="money-button" on:click={handlePresupuestoClick} on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && handlePresupuestoClick()} aria-label="Show Presupuesto Actual">
             <img src="/images/money-svg.svg" alt="money-svg">
@@ -91,7 +89,7 @@ onMount(() => {
     </div>
 
     <div class="section-content Recaudacion">
-        <h1>Recaudación</h1>
+        <h1 class="technic_head">Recaudación</h1>
         <div class="row">
         <button class="money-button-2" on:click={handleRecaudacionClick} on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && handleRecaudacionClick()} aria-label="Show Recaudacion Actual">
             <img src="/images/money-svg.svg" alt="money-svg">
@@ -105,43 +103,30 @@ onMount(() => {
     </div>
 
 </section>
-</div>
 </main>
 
 <style>
-.overlay-container {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 50vh;
-    background-color: rgb(255, 0, 0); /* Adjust transparency as needed */
-    z-index: 1; /* Ensure it overlays other content */
-}
 #section_header {
     color: aliceblue;
     font-size: 50px;
     text-align: center;
 }
-
-
-
 .scroll-container {
     display: flex;
     overflow-x: auto;
     scroll-behavior: smooth;
     width: 100%;
-    color: aliceblue;
+    height: 80vh;
+    color: aliceblue ;
+    z-index: 1000;
 }
-
 .section-content {
-    min-width: 70vw;
+    min-width: 100vw;
     display: flex;
     flex-direction: column;
     align-items: center;
     margin-top: 5%;
 }
-
 .row {
     display: flex;
     flex-direction: row;
@@ -149,18 +134,15 @@ onMount(() => {
     gap: 20px;
     margin-bottom: 2rem;
 }
-
 .technic_head {
     font-size: 80px;
     font-weight: 400;
-    margin: 0;
+    margin: 1;
 }
-
 h3 {
     font-size: 120px;
     margin: 80px 0;
 }
-
 .counter {
     font-size: 120px;
     margin: 0;
